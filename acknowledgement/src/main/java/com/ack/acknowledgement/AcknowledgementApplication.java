7d29eb3ee5a89ca0e0f398a0c413027d8a4d4838
parthyadav <parthyadav944@gmail.com>
2025-03-12 23:02:00 +0530

    starting the project

---
+++ acknowledgement/src/main/java/com/ack/acknowledgement/AcknowledgementApplication.java
@@ -0,0 +1,14 @@
+package com.ack.acknowledgement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcknowledgementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcknowledgementApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ acknowledgement/src/test/java/com/ack/acknowledgement/AcknowledgementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ack.acknowledgement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcknowledgementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
